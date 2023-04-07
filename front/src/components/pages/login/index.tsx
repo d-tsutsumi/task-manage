f1a39bf03670786757bfdab9d3637de5d963e174
大樹 堤 <daiju.tsutsumi@daiju.tsutsumi>
2023-04-07 15:52:44 +0900

    subscribeをusecaseに分離

--- front/src/components/pages/login/index.tsx
+++ front/src/components/pages/login/index.tsx
@@ -1,37 +1,23 @@
+import { FormEvent } from 'react';
+
 import LoginLayout from '../../layout/loginLayout';
 import LoginForm from './loginForm';
-import { FormEvent } from 'react';
-import { useTextInputValue } from '../../../hooks/useInputChange';
-import { login} from '@/usecase/login';
-import { setLoginUser, LoginUser, loginUser } from '../../../store/loginUser';
+import { useTextInputValue } from '@/hooks/useInputChange';
+
+import { login } from '@/usecase/login';
+import { loginUser } from '@/store/loginUser';
+import { loginSubscribe } from '@/usecase/login';
+import { useRouter } from 'next/router';
+
 export default function Login() {
   const [username, userNameSubject] = useTextInputValue();
   const [password, passwordSubject] = useTextInputValue();
+  const router = useRouter();
 
   const submit = (e: FormEvent) => {
-    console.log("gresgrse")
     e.preventDefault();
-    login(username, password).subscribe({
-      next: (res) => {
-        if (!res.error) {
-          const loginUser = res as LoginUser;
-          setLoginUser({
-            userName: loginUser.userName,
-            token: loginUser.token,
-            role: loginUser.role,
-          });
-        } else {
-          // TODO error処理
-          console.log(res);
-        }
-      },
-      complete: () => {
-        userNameSubject.complete();
-        passwordSubject.complete();
-        (document.getElementById('user_name') as HTMLInputElement).value = '';
-        (document.getElementById('password') as HTMLInputElement).value = '';
-      },
-    });
+    const subscribe = loginSubscribe(userNameSubject, passwordSubject, router);
+    login(username, password).subscribe(subscribe);
   };
 
   const loginFormProps = {
--- front/src/usecase/login/index.ts
+++ front/src/usecase/login/index.ts
@@ -1,7 +1,13 @@
 import loginRepository from '../../repositories/login';
 import { User } from '../../models/user';
-import { map, catchError, of } from 'rxjs';;
+import { map, catchError, of, Subject } from 'rxjs';
+import { LoginUser, setLoginUser } from '@/store/loginUser';
+import { FormEvent } from 'react';
+import { NextRouter } from 'next/router';
 
+type LoginResponse =
+  | { error: boolean; token: string; role: string; userName: string }
+  | { error: boolean; message: string };
 
 export const login = (user_name: string, password: string) =>
   loginRepository.post<User, { user_name: string; password: string }>({ user_name, password }).pipe(
@@ -12,3 +18,29 @@ export const login = (user_name: string, password: string) =>
     }),
   );
 
+export const loginSubscribe = (
+  userNameSubject: Subject<FormEvent<HTMLInputElement>>,
+  passwordSubject: Subject<FormEvent<HTMLInputElement>>,
+  router: NextRouter,
+) => ({
+  next: (res: LoginResponse) => {
+    if (!res.error) {
+      const loginUser = res as LoginUser;
+      setLoginUser({
+        userName: loginUser.userName,
+        token: loginUser.token,
+        role: loginUser.role,
+      });
+      router.push('/dashboad');
+    } else {
+      console.log('error');
+      // TODO error処理
+    }
+  },
+  complete: () => {
+    userNameSubject.complete();
+    passwordSubject.complete();
+    (document.getElementById('user_name') as HTMLInputElement).value = '';
+    (document.getElementById('password') as HTMLInputElement).value = '';
+  },
+});
